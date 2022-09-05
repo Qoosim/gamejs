56d5323e9b2eb6b9135a012aba3859ee51706dba
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-09-05 08:46:02 +0000

    Draw interactive circle

---
+++ canvas-basics/drawInteractiveCircle/script.js
@@ -0,0 +1,30 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+})
+
+const mouse = {
+  x: null,
+  y: null,
+}
+
+canvas.addEventListener('click', (event) => {
+  mouse.x = event.x;
+  mouse.y = event.y;
+  drawCircle();
+})
+
+function drawCircle() {
+  ctx.fillStyle = 'blue';
+  ctx.beginPath();
+  ctx.arc(mouse.x, mouse.y, 50, 0, Math.PI * 2);
+  ctx.fill();
+}
+
+
