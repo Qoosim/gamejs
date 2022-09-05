9c70194bfb9fca71e32747a4b6a4b3ca474ef588
Qoosim <ayinde.abdulghaniyy@gmail.com>
2022-09-05 08:32:56 +0000

    Draw circle with arc(), fillStroke, stroke(), lineWidth, beginPath()

---
+++ canvas-basics/drawCircle/script.js
@@ -0,0 +1,18 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+})
+
+ctx.fillStyle = 'blue';
+ctx.strokeStyle = 'red';
+ctx.lineWidth = 5;
+ctx.beginPath();
+ctx.arc(130, 100, 50, 0, Math.PI * 2);
+ctx.fill();
+ctx.stroke();
